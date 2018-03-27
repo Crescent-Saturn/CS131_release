a29501e4633118757f64c10faa97ce02c08b6dbe
Lei <Crescent-Saturn@users.noreply.github.com>
2018-03-27 16:41:31 -0400

    Harris done

--- hw3_release/panorama.py
+++ hw3_release/panorama.py
@@ -15,7 +15,7 @@ def harris_corners(img, window_size=3, k=0.04):
     Hint:
         You may use the function scipy.ndimage.filters.convolve, 
         which is already imported above
-        
+
     Args:
         img: Grayscale image of shape (H, W)
         window_size: size of the window function
@@ -33,9 +33,25 @@ def harris_corners(img, window_size=3, k=0.04):
     dx = filters.sobel_v(img)
     dy = filters.sobel_h(img)
 
-    ### YOUR CODE HERE
-    pass
-    ### END YOUR CODE
+    # YOUR CODE HERE
+    dx2 = dx ** 2
+    dy2 = dy ** 2
+    dxy = dx * dy
+
+    kernel = np.zeros((window_size, window_size))
+    sigma = 1
+
+    k = (window_size - 1) / 2
+    for i in range(window_size):
+        for j in range(window_size):
+            kernel[i, j] = np.exp(-((i - k)**2 + (j - k)**2) /
+                                  (2 * sigma**2)) / (2 * np.pi * sigma**2)
+    gdx2 = convolve(dx2, kernel)
+    gdy2 = convolve(dy2, kernel)
+    gdxy = convolve(dxy, kernel)
+
+    response = gdx2 * gdy2 - gdxy**2 - k * (gdx2 + gdy2)**2
+    # END YOUR CODE
 
     return response
 
@@ -45,23 +61,23 @@ def simple_descriptor(patch):
     Describe the patch by normalizing the image values into a standard 
     normal distribution (having mean of 0 and standard deviation of 1) 
     and then flattening into a 1D array. 
-    
+
     The normalization will make the descriptor more robust to change 
     in lighting condition.
-    
+
     Hint:
         If a denominator is zero, divide by 1 instead.
-    
+
     Args:
         patch: grayscale image patch of shape (h, w)
-    
+
     Returns:
         feature: 1D array of shape (h * w)
     """
     feature = []
-    ### YOUR CODE HERE
+    # YOUR CODE HERE
     pass
-    ### END YOUR CODE
+    # END YOUR CODE
     return feature
 
 
@@ -73,7 +89,7 @@ def describe_keypoints(image, keypoints, desc_func, patch_size=16):
         desc_func: function that takes in an image patch and outputs
             a 1D feature vector describing the patch
         patch_size: size of a square patch at each keypoint
-                
+
     Returns:
         desc: array of features describing the keypoints
     """
@@ -83,8 +99,8 @@ def describe_keypoints(image, keypoints, desc_func, patch_size=16):
 
     for i, kp in enumerate(keypoints):
         y, x = kp
-        patch = image[y-(patch_size//2):y+((patch_size+1)//2),
-                      x-(patch_size//2):x+((patch_size+1)//2)]
+        patch = image[y - (patch_size // 2):y + ((patch_size + 1) // 2),
+                      x - (patch_size // 2):x + ((patch_size + 1) // 2)]
         desc.append(desc_func(patch))
     return np.array(desc)
 
@@ -95,37 +111,37 @@ def match_descriptors(desc1, desc2, threshold=0.5):
     when the distance to the closest vector is much smaller than the distance to the 
     second-closest, that is, the ratio of the distances should be smaller
     than the threshold. Return the matches as pairs of vector indices.
-    
+
     Args:
         desc1: an array of shape (M, P) holding descriptors of size P about M keypoints
         desc2: an array of shape (N, P) holding descriptors of size P about N keypoints
-        
+
     Returns:
         matches: an array of shape (Q, 2) where each row holds the indices of one pair 
         of matching descriptors
     """
     matches = []
-    
+
     N = desc1.shape[0]
     dists = cdist(desc1, desc2)
 
-    ### YOUR CODE HERE
+    # YOUR CODE HERE
     pass
-    ### END YOUR CODE
-    
+    # END YOUR CODE
+
     return matches
 
 
 def fit_affine_matrix(p1, p2):
     """ Fit affine matrix such that p2 * H = p1 
-    
+
     Hint:
         You can use np.linalg.lstsq function to solve the problem. 
-        
+
     Args:
         p1: an array of shape (M, P)
         p2: an array of shape (M, P)
-        
+
     Return:
         H: a matrix of shape (P * P) that transform p2 to p1.
     """
@@ -135,13 +151,13 @@ def fit_affine_matrix(p1, p2):
     p1 = pad(p1)
     p2 = pad(p2)
 
-    ### YOUR CODE HERE
+    # YOUR CODE HERE
     pass
-    ### END YOUR CODE
+    # END YOUR CODE
 
     # Sometimes numerical issues cause least-squares to produce the last
     # column which is not exactly [0, 0, 1]
-    H[:,2] = np.array([0, 0, 1])
+    H[:, 2] = np.array([0, 0, 1])
     return H
 
 
@@ -170,20 +186,20 @@ def ransac(keypoints1, keypoints2, matches, n_iters=200, threshold=20):
     N = matches.shape[0]
     n_samples = int(N * 0.2)
 
-    matched1 = pad(keypoints1[matches[:,0]])
-    matched2 = pad(keypoints2[matches[:,1]])
+    matched1 = pad(keypoints1[matches[:, 0]])
+    matched2 = pad(keypoints2[matches[:, 1]])
 
     max_inliers = np.zeros(N)
     n_inliers = 0
 
     # RANSAC iteration start
-    ### YOUR CODE HERE
+    # YOUR CODE HERE
     pass
-    ### END YOUR CODE
+    # END YOUR CODE
     return H, matches[max_inliers]
 
 
-def hog_descriptor(patch, pixels_per_cell=(8,8)):
+def hog_descriptor(patch, pixels_per_cell=(8, 8)):
     """
     Generating hog descriptor by the following steps:
 
@@ -200,16 +216,16 @@ def hog_descriptor(patch, pixels_per_cell=(8,8)):
         block: 1D array of shape ((h*w*n_bins)/(m*n))
     """
     assert (patch.shape[0] % pixels_per_cell[0] == 0),\
-                'Heights of patch and cell do not match'
+        'Heights of patch and cell do not match'
     assert (patch.shape[1] % pixels_per_cell[1] == 0),\
-                'Widths of patch and cell do not match'
+        'Widths of patch and cell do not match'
 
     n_bins = 9
     degrees_per_bin = 180 // n_bins
 
     Gx = filters.sobel_v(patch)
     Gy = filters.sobel_h(patch)
-   
+
     # Unsigned gradients
     G = np.sqrt(Gx**2 + Gy**2)
     theta = (np.arctan2(Gy, Gx) * 180 / np.pi) % 180
@@ -222,8 +238,8 @@ def hog_descriptor(patch, pixels_per_cell=(8,8)):
     cells = np.zeros((rows, cols, n_bins))
 
     # Compute histogram per cell
-    ### YOUR CODE HERE
+    # YOUR CODE HERE
     pass
-    ### YOUR CODE HERE
-    
+    # YOUR CODE HERE
+
     return block
